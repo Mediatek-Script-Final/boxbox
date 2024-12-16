aefea37a4ac65c448886c3ba55f323a32bbbd1d8
Dexter Eskalarte <167977652+Mediatek-Script-Final@users.noreply.github.com>
2024-12-16 10:24:11 +0800

    Update siteOption.js

--- src/contentData/siteOption.js
+++ src/contentData/siteOption.js
@@ -3,7 +3,7 @@ const siteOptionData = [
       id: 1,
       siteName: "BLOODLINE DEV",
       image:
-        "https://ibb.co/6NhJ7hy",
+        "https://i.ibb.co/1RF1Gfq/favicon.png",
       description: "Experience a secure and reliable VPN service with enhanced privacy and performance.",
      // siteLink: "https://starlinkbrand.xyz/login"
     }
